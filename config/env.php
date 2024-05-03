2ff659136ade2eb7147a59042ebb2f15e2c86ba6
rekaananda1019 <146187561+rekaananda1019@users.noreply.github.com>
2024-05-04 00:59:41 +0700

    Update env.php

--- config/env.php
+++ config/env.php
@@ -1,4 +1,17 @@
 <?php
-// Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+function parseEnv($filePath){
+    $contents = file_get_contents($filePath);
+    $lines = explode("\n", $contents);
+    $env = [];
+
+    foreach($lines as $line){
+        $line = trim($line);
+        if(!empty($line) && strpos($line, '=') !== false) {
+            list($key, $value) = explode('=', $line,2);
+            $env[$key] = $value;
+        }
+    }
+    return $env;
+}
+
+$_ENV = parseEnv(".env");
